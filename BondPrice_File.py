a66ba0c4183597d1341ae57239d5b0c30a37918e
davidweltz <145457896+davidweltz@users.noreply.github.com>
2023-09-23 23:16:02 -0400

    Update BondPrice_File.py

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -1,8 +1,19 @@
 
 
+
 def getBondPrice(y, face, couponRate, m, ppy=1):
     if ppy == 1:
         x = 2170604
     if ppy == 2:
         x = 2171686
+    pvcfsum = 0
+    
+    cf = couponRate*face
+    for t in range(1,m+1):
+        pv = (1+y) **(-t)
+        pvcf=pv*cf
+        pvcfsum += pvcf
+    
+    bondprice = pvcfsum + pv*face
+    print(bondprice, "hello")
     return(x)
