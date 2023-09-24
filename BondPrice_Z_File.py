f35034c54886fecf93e51ec2f08d7c159f9c18dc
davidweltz <145457896+davidweltz@users.noreply.github.com>
2023-09-24 09:59:35 -0400

    Update BondPrice_Z_File.py

--- BondPrice_Z_File.py
+++ BondPrice_Z_File.py
@@ -2,3 +2,13 @@
 
 def getBondPrice_Z(face, couponRate, times, yc):
     return(1996533)
+
+    cf = couponRate*face
+    for t in range(len(yc)):
+        pv = (1+yc[t]) **(-m[t])
+        pvcf=pv*cf
+        pvcfsum += pvcf
+    
+    bondprice = pvcfsum + pv*face
+    print(bondprice, "hello")
+     return(bondprice)
