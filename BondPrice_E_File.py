3be45e9768499ea49696bd9c7cef8168a70b76e2
davidweltz <145457896+davidweltz@users.noreply.github.com>
2023-09-24 22:35:52 -0400

    Update BondPrice_E_File.py

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -1,4 +1,15 @@
 
 
 def getBondPrice_E(face, couponRate, m, yc):
-    return(2098949)
+    pvcfsum = 0
+    
+    cf = couponRate*face
+    for t in range(1,m+1):
+        pv = (1+yc[t-1]) **(-t)
+        pvcf=pv*cf
+        pvcfsum += pvcf
+    
+    bondprice = pvcfsum + pv*face
+    #print(bondprice, "hello")
+    return(bondprice)
+    #return(2098949)
