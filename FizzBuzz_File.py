b1347ba423c89d753bb55d8fb31fbf92aff94a6c
davidweltz <145457896+davidweltz@users.noreply.github.com>
2023-09-24 23:10:46 -0400

    Update FizzBuzz_File.py

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -3,11 +3,11 @@
 def FizzBuzz(start, finish):
     v=[]
     for i in range(start, finish+1):
-        if i % 15== 0:
+        if i % 15 == 0:
             v.append('fizzbuzz')
         elif i % 5 == 0:
               v.append('buzz')
-        elif i % 3== 0:
+        elif i % 3 == 0:
               v.append('fizz')
         else:
             v.append(i)
