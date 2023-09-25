c3f9ea87855cc720108dad3ef3afa8653a3b16ff
davidweltz <145457896+davidweltz@users.noreply.github.com>
2023-09-24 23:09:04 -0400

    Update FizzBuzz_File.py

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -1,15 +1,16 @@
 
 
 def FizzBuzz(start, finish):
-    for i = start to finish+1:
-        if i mod 15:
+    v=[]
+    for i in range(start, finish+1):
+        if i % 15== 0:
             v.append('fizzbuzz')
-        elif i mod 5:
-              v.extend('buzz')
-        elif i mod 3:
-              v.extend('fizz')
+        elif i % 5 == 0:
+              v.append('buzz')
+        elif i % 3== 0:
+              v.append('fizz')
         else:
-            v.extend('fizz')
+            v.append(i)
 
     #v = ['buzz', 41, 'fizz', 43, 44, 'fizzbuzz']
     return(v)
